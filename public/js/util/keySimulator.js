33fcf88bd7b37b0abe2bb57e8ca1c98c9e98c694
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-14 21:43:33 -0500

    Plus / Minus key now toggles pos / neg state

--- public/js/util/keySimulator.js
+++ public/js/util/keySimulator.js
@@ -1,3 +1,11 @@
 const keySimulator = (keyValue) => {
   window.dispatchEvent(new KeyboardEvent("keydown", { key: keyValue }));
 };
+
+const plusMinusKey = () => {
+  const currExprDisp = document.querySelector("#current-expression");
+  const currentContent = currExprDisp.innerHTML;
+  currExprDisp.innerHTML = currentContent.includes("-")
+    ? currentContent.substring(1)
+    : `-${currentContent}`;
+};
