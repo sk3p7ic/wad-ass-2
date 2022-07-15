347e150fe29e36fc2b36d57dca44e3c7f90e4c71
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-14 20:44:37 -0500

    Implemented math functions

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -8,6 +8,39 @@ const currExprDisp = document.querySelector("#current-expression");
 
 const _startEvaluation = (expr) => {
   console.log(expr);
+  const exprArr = expr.split(" ");
+  const value1 = exprArr[0];
+  const operand = exprArr[1];
+  const value2 = exprArr[2];
+  console.log(value1, value2);
+  let result;
+  switch (operand) {
+    case "+":
+      result = parseFloat(value1) + parseFloat(value2);
+      currExprDisp.innerHTML = result;
+      break;
+    case "-":
+      result = parseFloat(value1) - parseFloat(value2);
+      currExprDisp.innerHTML = result;
+      break;
+    case "*":
+      result = parseFloat(value1) * parseFloat(value2);
+      currExprDisp.innerHTML = result;
+      break;
+    case "/":
+      result = parseFloat(value1) / parseFloat(value2);
+      currExprDisp.innerHTML = result;
+      break;
+    case "%":
+      result = parseFloat(value1) % parseFloat(value2);
+      currExprDisp.innerHTML = result;
+      break;
+    case "^":
+      result = Math.pow(parseFloat(value1), parseFloat(value2));
+      currExprDisp.innerHTML = result;
+      break;
+  }
+  console.log(result);
 };
 
 addRestrictedKeyboardTypingListener(currExprDisp, prevExprDisp);
