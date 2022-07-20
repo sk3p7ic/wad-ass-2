674573d51f420ed553d86d967acda7f308544f54
Joshua Ibrom <joshuaibrom@gmail.com>
2022-07-20 00:34:55 -0500

    Calc history now works

--- public/js/util/historyDisplay.js
+++ public/js/util/historyDisplay.js
@@ -5,15 +5,45 @@ const historyBtn = document.querySelector("#history-btn");
 let showHistory = false;
 let history = {};
 
+const _renderHistory = () => {
+  const itemsContainer = historyDiv.querySelector("#history-items");
+  itemsContainer.innerHTML = ""; // Reset the contents of the container
+  for (const [expression, result] of Object.entries(history)) {
+    let btn = document.createElement("button");
+    btn.onclick = () =>
+      (document.querySelector("#current-expression").innerHTML = result);
+    btn.classList.add(
+      "btn",
+      "btn-primary",
+      "d-flex",
+      "flex-row",
+      "justify-content-between"
+    );
+    const exprPara = document.createElement("p");
+    exprPara.innerHTML = expression;
+    btn.appendChild(exprPara);
+    const resPara = document.createElement("p");
+    resPara.innerHTML = result;
+    btn.appendChild(resPara);
+    itemsContainer.appendChild(btn);
+  }
+};
+
 const _toggleShowHistory = () => {
   showHistory = !showHistory; // Toggle the history
   const outputHeight = outputDisp.getBoundingClientRect().height;
   outputDisp.classList.toggle("d-none");
+  historyBtn.classList.toggle("active");
   historyDiv.classList.toggle("d-none");
   if (showHistory) historyDiv.style.height = `${outputHeight}px`;
+  _renderHistory();
 };
 
 export const setupHistoryButton = () => {
   if (!showHistory) historyDiv.classList.add("d-none");
   historyBtn.onclick = _toggleShowHistory;
+};
+
+export const updateHistory = (expression, result) => {
+  history = { ...history, [expression]: result };
 };
